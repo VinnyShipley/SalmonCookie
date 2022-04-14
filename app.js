40db96c6eb6e0ac475e3d099208c84e791c6f079
Vinny <vinnyshipley@gmail.com>
2022-04-13 21:37:47 -0700

    table closer

--- app.js
+++ app.js
@@ -21,14 +21,13 @@ City.prototype.generateCustPerHour = function(){
     return range;
 }
 
-
 //returns cookies sold per hour
 City.prototype.cookiePerHour = function (){
   let cookieSoldHourly = Math.ceil(this.custPerHour*this.avgCookiePerGuest);
   return cookieSoldHourly;
 }
 
-//returns array of cookie hourly sold
+//returns array of cookie hourly sold. needs fixing, only displaying one number
 City.prototype.cookiePerDay = function (){
   for (let i=0; i <= 13; i++){
     this.cookiesSoldDailyArray.push(this.cookiePerHour());
@@ -36,43 +35,102 @@ City.prototype.cookiePerDay = function (){
 }
 
 //Rendered text on page
-City.prototype.render = function(){
-  const containerElem = document.getElementById('cityEstimates');
+//links to cityEstimates in sales.html
+const containerElem = document.getElementById('cityEstimates');
 
-  const articleElem = document.createElement('article');
-  containerElem.appendChild(articleElem);
+//creates table element
+const tableElem = document.createElement('table');
+containerElem.appendChild(tableElem);
 
-  //City Name
-  const headingElem = document.createElement('h2');
-  articleElem.appendChild(headingElem);
-  headingElem.textContent = `${this.name}`;
+//creates the row that houses the hour slots
+const timeRowElem = document.createElement('tr');
+tableElem.appendChild(timeRowElem);
 
-  //Table of Projected Sales by Hour
-  const tableElem = document.createElement('table');
-  articleElem.appendChild(tableElem);
+//prints to screen the time slots
+const timeRowHeading = document.createElement('th');
+timeRowElem.appendChild(timeRowHeading);
+for(let i = 0; i<timeArray.length; i++){
+  const timeRowContent = document.createElement('td')
+  timeRowHeading.appendChild(timeRowContent);
+  timeRowContent.textContent = `${timeArray[i]}`;
+};
 
+//creates the cookie sales per hour row
+const salesRowElem = document.createElement('tr');
+tableElem.appendChild(salesRowElem);
 
-  //table header row
-  function timeRowTitles(){
-    for(let i = 0; i < timeArray.length; i++){
-      const timeHeaderRow = document.createElement('th');
-      tableElem.appendChild(timeHeaderRow);
-      timeHeaderRow.textContent = `${timeArray[i]}`;
-    }
-  }
-  timeRowTitles();
+//prints to screen the name of the city
 
-  // cookies sold hourly row 
-  // function salesRowValues(){
-  //   for(let i = 0; i < timeArray.length; i++){
-  //     const salesValueRow = document.createElement('td');
-  //     tableElem.appendChild(salesValueRow);
-  //     salesValueRow.textContent = `${this.cookiesSoldDailyArray};`;
-  //   }
-  // }
-  // salesRowValues();
+const cityRowHeading = document.createElement('th');
+  salesRowElem.appendChild(cityRowHeading);
 
+for(let i = 0; i<timeArray.length; i++){
+  const cityRowContent = document.createElement('td');
+  cityRowHeading.appendChild(cityRowContent);
+  cityRowContent.textContent = `${`random`}`;
 }
+
+
+
+
+
+
+
+
+
+
+
+// const containerElem = document.getElementById('cityEstimates');
+// const articleElem = document.createElement('article');
+// containerElem.appendChild(articleElem);
+
+// //Whole table element
+// const tableElem = document.createElement('table');
+// articleElem.appendChild(tableElem);
+
+
+// //table body element where daily raw data is stored
+// const tableBody = document.createElement('th');
+// tableElem.appendChild(tableBody);
+
+// //header of the time table row
+// const timeTableHeader = document.createElement('th');
+// articleElem.appendChild(timeTableHeader);
+
+
+
+// //creates the time row of table/ prints it to page
+// for(let i = 0; i < timeArray.length; i++){
+//   const timeHeaderRow = document.createElement('td');
+//   timeTableHeader.appendChild(timeHeaderRow);
+//   timeHeaderRow.textContent = `${timeArray[i]}`;
+// }
+
+
+// //creates row that the sales values will go into
+// const newDataRow = document.createElement('tr');
+// tableElem.appendChild(newDataRow);
+
+
+
+// //looping text render
+// City.prototype.render = function(){
+  
+//   //City Name row
+//   const headingElem = document.createElement('h2');
+//   timeTableHeader.appendChild(headingElem);
+//   timeTableHeader.textContent = `${this.name}`;
+
+//   //cookies sold hourly row 
+//   for(let i = 0; i < timeArray.length; i++){
+//     const newDataRow = document.createElement('td');
+//     tableBody.appendChild(newDataRow);
+//     newDataRow.textContent = this.cookiesSoldDailyArray[i];
+//   }
+  
+
+
+//}
 
 
 const seattle = new City ('Seattle', 23, 65, 6.3);
@@ -80,6 +138,6 @@ const tokyo = new City ('Tokyo', 3, 24, 1.2);
 
 
 seattle.cookiePerDay();
-tokyo.cookiePerDay();
-seattle.render();
-tokyo.render();
+// seattle.render();
+// tokyo.cookiePerDay();
+// tokyo.render();
