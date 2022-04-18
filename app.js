a6d4a77e0da01c4b9f0ab50fadc296d65e6f3c0d
Vinny <vinnyshipley@gmail.com>
2022-04-17 21:34:16 -0700

    table and some total functionality achieved, still needs formatting fixes

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ function CookieStand(name, minCustAllDay, maxCustAllDay, avgCookiePerGuest) {
   this.avgCookiePerGuest = avgCookiePerGuest;
   this.custPerHour = this.generateCustPerHour();
   this.cookiesSoldDailyArray = this.generateCookiesPerHour();
-  // this.totalCookieByLocation();
+  this.allDayTotalOneLocation = this.generateAllDayTotalOneLocation();
+  
 }
 
 //list of times
@@ -22,17 +23,25 @@ CookieStand.prototype.generateCustPerHour = function () {
 }
 
 
+
 //returns cookies sold per hour
 CookieStand.prototype.generateCookiesPerHour = function () {
   let singleHourCookieSales;
   let cookieHourlyArray = [];
   for (let i = 0; i < timeArray.length; i++){
     singleHourCookieSales = Math.ceil(this.generateCustPerHour() * this.avgCookiePerGuest);
-    cookieHourlyArray.push(singleHourCookieSales);}
-
+    cookieHourlyArray.push(singleHourCookieSales);
+  }
   return cookieHourlyArray;
 }
 
+//Finds sum of total cookies sold at one location all day
+CookieStand.prototype.generateAllDayTotalOneLocation = function (){
+  let sumOfCookieArray = this.cookiesSoldDailyArray.reduce(function(a, b){
+    return a + b;
+  }, 0);
+  return sumOfCookieArray;
+}
 
 //Rendered text on page
 //links to CookieStandEstimates in sales.html
@@ -46,10 +55,6 @@ containerElem.appendChild(tableElem);
 const timeRowElem = document.createElement('tr');
 tableElem.appendChild(timeRowElem);
 
-//prints to screen the time slots
-const timeRowHeading = document.createElement('th');
-timeRowElem.appendChild(timeRowHeading);
-
 //creates spaceholder cell so that the table lines up with itself
 const spaceHolderCell = document.createElement('td');
 tableElem.appendChild(spaceHolderCell);
@@ -61,12 +66,12 @@ for (let i = 0; i < timeArray.length; i++) {
   timeRowContent.textContent = `${timeArray[i]}`;
 };
 
-
-//looping text render
+//looping text render of the city and the hourly sale of one location
 CookieStand.prototype.render = function(){
   const newDataRow = document.createElement('tr');
   tableElem.appendChild(newDataRow);
-  //CookieStand Name row
+
+  //cookie stand city name
   const cityCell = document.createElement('td');
   newDataRow.appendChild(cityCell);
   cityCell.textContent = `${this.name}`;
@@ -75,27 +80,81 @@ CookieStand.prototype.render = function(){
   for(let i = 0; i < timeArray.length; i++){
     const salesCellValues = document.createElement('td');
     newDataRow.appendChild(salesCellValues);
-    salesCellValues.textContent = `${this.cookiesSoldDailyArray[i]}`
-  }
+    salesCellValues.textContent = `${this.cookiesSoldDailyArray[i]}`;
+  } 
+
+  
+  // adds all day totals for single location into cell
+      const totalOneLocationAllDayCell = document.createElement('td');
+      newDataRow.appendChild(totalOneLocationAllDayCell);
+      totalOneLocationAllDayCell.textContent = `${this.allDayTotalOneLocation}`;
+}
+
+//adds cell above the daily totals
+ const labelOfTotalByLocationPerDayColumn = document.createElement('td');
+timeRowElem.appendChild(labelOfTotalByLocationPerDayColumn);
+labelOfTotalByLocationPerDayColumn.textContent = `totals`;
+
+//creates row that total by hour at location will live in
+const totalRowElem = document.createElement('tr');
+tableElem.appendChild(totalRowElem);
+
+//creates blank spaceholder cell for total by hourly row 
+const hourlyPlaceholderCell = document.createElement('td');
+totalRowElem.appendChild(hourlyPlaceholderCell);
+
+
+//populates the total by hour row
+for (let i = 0; i < timeArray.length; i++){
+const hourlyTotalCell = document.createElement('td');
+totalRowElem.appendChild(hourlyTotalCell);
+hourlyTotalCell.textContent = `Hourly Totals Needed`;
+}
+
+let cookieStands = [];
+
+function createCookieStands(){
+  const seattle = new CookieStand('Seattle', 23, 65, 6.3)
+  seattle.render();
+  cookieStands.push(seattle);
+
+  const tokyo = new CookieStand('Tokyo', 3, 24, 1.2)
+  tokyo.render();
+  cookieStands.push(tokyo);
+
+  const dubai = new CookieStand('Dubai', 11, 38, 3.7);
+  dubai.render();
+  cookieStands.push(dubai);
+
+  const paris = new CookieStand('Paris', 20, 38, 2.3);
+  paris.render();
+  cookieStands.push(paris);
+
+  const lima = new CookieStand('Lima', 2, 16, 4.6);
+  lima.render();
+  cookieStands.push(lima);
 }
 
 
-//creates the cookie sales per hour row
-const salesRowElem = document.createElement('tr');
-tableElem.appendChild(salesRowElem);
 
-//creates heading for sales row
-const salesRowHeading = document.createElement('th');
-salesRowElem.appendChild(salesRowHeading);
+//New submission input function
 
-const seattle = new CookieStand('Seattle', 23, 65, 6.3);
-const tokyo = new CookieStand('Tokyo', 3, 24, 1.2);
-const dubai = new CookieStand('Dubai', 11, 38, 3.7);
-const paris = new CookieStand('Paris', 20, 38, 2.3)
-const lima = new CookieStand('Lima', 2, 16, 4.6)
+function handleSubmit(event){
+  const cityInputElem = event.target.cityname.value;
+  const newMinCustPerHour = parseInt(event.target.minCust.value);
+  const newMaxCustomerPerHour = parseInt(event.target.maxCust.value);
+  const newAvgCookiePerGuest = parseFloat(event.target.avgCookiePerGuest.value);
+ 
+  let newCityArray = [cityInputElem, newMinCustPerHour, newMaxCustomerPerHour, newAvgCookiePerGuest];
+  return cookieStands.push(handleSubmit);
 
-seattle.render();
-tokyo.render();
-dubai.render();
-paris.render();
-lima.render();
+}
+console.log(handleSubmit.event)
+
+
+const formElem = document.getElementById('newCityInput');
+formElem.addEventListener('submit', handleSubmit);
+
+
+//populates the sales data into the table
+createCookieStands();
